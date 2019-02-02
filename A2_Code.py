2b0b6a17f9db4e013223ab3a70e2954f97643cf4
Yawen Duan <35738259+kmdanielduan@users.noreply.github.com>
2019-02-02 16:38:38 -0600

    Add files via upload

---
+++ A2_Code.py
@@ -0,0 +1,185 @@
+import numpy as np
+import h5py
+#data file type h5py
+import time
+import copy
+from random import randint
+
+# cd Desktop/CS\ 398/Assignments/A2/
+#load MNIST data
+MNIST_data = h5py.File('MNISTdata.hdf5', 'r')
+x_train = np.float32(MNIST_data['x_train'][:])
+y_train = np.int32(np.array(MNIST_data['y_train'][:,0]))
+x_test = np.float32(MNIST_data['x_test'][:])
+y_test = np.int32(np.array(MNIST_data['y_test'][:,0]))
+MNIST_data.close()
+
+####################################################################################
+#Implementation of stochastic gradient descent algorithm
+
+class NN:
+    first_layer = {}
+    second_layer = {}
+
+    def __init__(self, inputs, hidden, outputs):
+        # initialize the model parameters, including the first and second layer 
+        # parameters and biases
+        self.first_layer['para'] = np.random.randn(hidden,inputs) / np.sqrt(num_inputs)
+        self.first_layer['bias'] = np.random.randn(hidden,1) / np.sqrt(hidden)
+        self.second_layer['para'] = np.random.randn(outputs,hidden) / np.sqrt(hidden)
+        self.second_layer['bias'] = np.random.randn(outputs,1) / np.sqrt(hidden)
+        self.input_size = inputs
+        self.hid_size = hidden
+        self.output_size = outputs
+
+    def __activfunc(self,Z,type = 'ReLU'):
+        # implement the activation function
+        if type == 'ReLU':
+            return np.array([i if i>0 else 0 for i in np.squeeze(Z)])
+        elif type == 'Sigmoid':
+            return 1/(1+np.exp(-Z))
+        elif type == 'tanh':
+            return np.tanh(Z)
+        else:
+            raise TypeError('Invalid type!')
+
+    def __Softmax(self,z):
+        # implement the softmax function
+        return 1/sum(np.exp(z)) * np.exp(z)
+
+    def __cross_entropy_error(self,v,y):
+        # implement the cross entropy error
+        return -np.log(v[y])
+
+    def __forward(self,x,y):
+        # implement the forward computation, calculation of prediction list and error
+        Z = np.matmul(self.first_layer['para'],x).reshape((self.hid_size,1)) + self.first_layer['bias']
+        H = np.array(self.__activfunc(Z)).reshape((self.hid_size,1))
+        U = np.matmul(self.second_layer['para'],H).reshape((self.output_size,1)) + self.second_layer['bias']
+        predict_list = np.squeeze(self.__Softmax(U))
+        error = self.__cross_entropy_error(predict_list,y)
+        
+        dic = {
+            'Z':Z,
+            'H':H,
+            'U':U,
+            'f_X':predict_list.reshape((1,self.output_size)),
+            'error':error
+        }
+        return dic
+
+    def __back_propagation(self,x,y,f_result):
+        # implement the back propagation process, compute the gradients
+        E = np.array([0]*self.output_size).reshape((1,self.output_size))
+        E[0][y] = 1
+        dU = (-(E - f_result['f_X'])).reshape((self.output_size,1))
+        db_2 = copy.copy(dU)
+        dC = np.matmul(dU,f_result['H'].transpose())
+        delta = np.matmul(self.second_layer['para'].transpose(),dU)
+        db_1 = delta.reshape(self.hid_size,1)*self.__activfunc(f_result['Z']).reshape(self.hid_size,1)
+        dW = np.matmul(db_1.reshape((self.hid_size,1)),x.reshape((1,784)))
+
+        grad = {
+            'dC':dC,
+            'db_2':db_2,
+            'db_1':db_1,
+            'dW':dW
+        }
+        return grad
+
+    def __optimize(self,b_result, learning_rate):
+        # update the hyperparameters
+        self.second_layer['para'] -= learning_rate*b_result['dC']
+        self.second_layer['bias'] -= learning_rate*b_result['db_2']
+        self.first_layer['bias'] -= learning_rate*b_result['db_1']
+        self.first_layer['para'] -= learning_rate*b_result['dW']
+
+    def __loss(self,X_train,Y_train):
+        # implement the loss function of the training set
+        loss = 0
+        for n in range(len(X_train)):
+            y = Y_train[n]
+            x = X_train[n][:]
+            loss += self.__forward(x,y)['error']
+        return loss
+
+    def train(self, X_train, Y_train, num_iterations = 1000, learning_rate = 0.5):
+        # generate a random list of indices for the training set
+        rand_indices = np.random.choice(len(X_train), num_iterations, replace=True)
+        
+        def l_rate(base_rate, ite, num_iterations, schedule = False):
+        # determine whether to use the learning schedule
+            if schedule == True:
+                return base_rate * 10 ** (-np.floor(ite/num_iterations*5))
+            else:
+                return base_rate
+
+        count = 1
+        loss_dict = {}
+        test_dict = {}
+
+        for i in rand_indices:
+            f_result = self.__forward(X_train[i],Y_train[i])
+            b_result = self.__back_propagation(X_train[i],Y_train[i],f_result)
+            self.__optimize(b_result,l_rate(learning_rate,i,num_iterations,True))
+            
+            if count % 1000 == 0:
+                if count % 5000 == 0:
+                    loss = self.__loss(X_train,Y_train)
+                    test = self.testing(x_test,y_test)
+                    print('Trained for {} times,'.format(count),'loss = {}, test = {}'.format(loss,test))
+                    loss_dict[str(count)]=loss
+                    test_dict[str(count)]=test
+                else:
+                    print('Trained for {} times,'.format(count))
+            count += 1
+
+        print('Training finished!')
+        return loss_dict, test_dict
+
+    def testing(self,X_test, Y_test):
+        # test the model on the training dataset
+        total_correct = 0
+        for n in range(len(X_test)):
+            y = Y_test[n]
+            x = X_test[n][:]
+            prediction = np.argmax(self.__forward(x,y)['f_X'])
+            if (prediction == y):
+                total_correct += 1
+        print('Accuarcy Test: ',total_correct/len(X_test))
+        return total_correct/np.float(len(X_test))
+
+####################################################################################
+
+# set the number of iterations
+num_iterations = 200000
+# set the base learning rate
+learning_rate = 0.01
+# number of inputs
+num_inputs = 28*28
+# number of outputs
+num_outputs = 10
+# size of hidden layer
+hidden_size = 300
+
+# data fitting, training and accuracy evaluation
+model = NN(num_inputs,hidden_size,num_outputs)
+cost_dict, tests_dict = model.train(x_train,y_train,num_iterations=num_iterations,learning_rate=learning_rate)
+accu = model.testing(x_test,y_test)
+
+
+# plotting the loss function and test accuracy corresponding to the number of iterations
+import matplotlib.pyplot as plt
+plt.plot(cost_dict.keys(),cost_dict.values())
+plt.ylabel('Loss function')
+plt.xlabel('Number of iterations')
+plt.xticks(rotation=60)
+plt.title('Loss function w.r.t. number of iterations')
+plt.show()
+
+plt.plot(tests_dict.keys(),tests_dict.values())
+plt.ylabel('Test Accuracy')
+plt.xlabel('Number of iterations')
+plt.xticks(rotation=60)
+plt.title('Test accuracy w.r.t. number of iterations')
+plt.show()
